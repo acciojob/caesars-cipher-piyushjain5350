10108976c276992dbe0533dda699f7e19f3417ff
piyushjain5350 <126599802+piyushjain5350@users.noreply.github.com>
2023-06-14 17:28:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -35,7 +35,8 @@ function rot13(encodedStr) {
   let decodedArr = []; // Your Result goes here
   // Only change code below this line
 	for(let i=0;i<encodedStr.length;i++){
-		decodedArr.push(lookup.encodedStr.charAt(i));
+		let a=encodedStr.charAt(i);
+		decodedArr.push(lookup.a);
 	}
 
   return decodedArr; //return decodedArr
